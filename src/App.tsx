88fde3907f5dc1fcd733e63925f27717c1febe52
hiimsewek <102280@stud.uz.zgora.pl>
2024-10-10 17:53:57 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "styles/globalStyles.scss";
+
+const App = () => {
+  return <div>Memory Game</div>;
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      strategies: "generateSW",
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      workbox: {
+        globPatterns: ["**/*"],
+      },
+      includeAssets: ["**/*"],
+    }),
+  ],
+  resolve: {
+    alias: {
+      assets: "/src/assets",
+      components: "/src/components",
+      data: "/src/data",
+      store: "/src/store",
+      styles: "/src/styles",
+      types: "/src/types",
+      utils: "/src/utils",
+    },
+  },
+});
