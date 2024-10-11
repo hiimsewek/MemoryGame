b713d7fa556a5fea3dae47bc2634533c21617a0b
hiimsewek <62641653+hiimsewek@users.noreply.github.com>
2024-10-12 00:34:24 +0200

    Add card and game related types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,20 @@
-export {};
+export type Category = "animals" | "fruits" | "sports";
+
+export type Card = {
+  type: Category;
+  image: string;
+  value: string;
+};
+
+export type CardWithId = Card & {
+  id: string;
+};
+
+export type Difficulty = "easy" | "medium" | "hard";
+
+export type GameInfo = {
+  attempts: string;
+  duration: number;
+  date: Date;
+  difficulty: Difficulty;
+}[];
