3389a7b9165fd46d0fb518dc5e281e6259f36ec2
hiimsewek <62641653+hiimsewek@users.noreply.github.com>
2024-10-12 00:31:00 +0200

    Add cards data

---
+++ src/data/cards.ts
@@ -0,0 +1,244 @@
+import { Card } from "types";
+
+export const cards: Card[] = [
+  {
+    type: "animals",
+    image: "/assets/animals/bear.jpg",
+    value: "bear",
+  },
+  {
+    type: "animals",
+    image: "/assets/animals/bird.jpg",
+    value: "bird",
+  },
+  {
+    type: "animals",
+    image: "/assets/animals/cat.jpg",
+    value: "cat",
+  },
+  {
+    type: "animals",
+    image: "/assets/animals/chicken.jpg",
+    value: "chicken",
+  },
+  {
+    type: "animals",
+    image: "/assets/animals/deer.jpg",
+    value: "deer",
+  },
+  {
+    type: "animals",
+    image: "/assets/animals/dog.jpg",
+    value: "dog",
+  },
+  {
+    type: "animals",
+    image: "/assets/animals/egret.jpg",
+    value: "egret",
+  },
+  {
+    type: "animals",
+    image: "/assets/animals/giraffe.jpg",
+    value: "giraffe",
+  },
+  {
+    type: "animals",
+    image: "/assets/animals/hedgehog.jpg",
+    value: "hedgehog",
+  },
+  {
+    type: "animals",
+    image: "/assets/animals/lioness.jpg",
+    value: "lioness",
+  },
+  {
+    type: "animals",
+    image: "/assets/animals/parrot.jpg",
+    value: "parrot",
+  },
+  {
+    type: "animals",
+    image: "/assets/animals/squirrel.jpg",
+    value: "squirrel",
+  },
+  {
+    type: "animals",
+    image: "/assets/animals/swan.jpg",
+    value: "swan",
+  },
+  {
+    type: "animals",
+    image: "/assets/animals/turtle.jpg",
+    value: "turtle",
+  },
+  {
+    type: "animals",
+    image: "/assets/animals/wolf.jpg",
+    value: "wolf",
+  },
+  {
+    type: "animals",
+    image: "/assets/animals/zebra.jpg",
+    value: "zebra",
+  },
+  {
+    type: "fruits",
+    image: "/assets/fruits/apples.jpg",
+    value: "apple",
+  },
+  {
+    type: "fruits",
+    image: "/assets/fruits/bananas.jpg",
+    value: "banana",
+  },
+  {
+    type: "fruits",
+    image: "/assets/fruits/blueberries.jpg",
+    value: "blueberry",
+  },
+  {
+    type: "fruits",
+    image: "/assets/fruits/cherries.jpg",
+    value: "cherry",
+  },
+  {
+    type: "fruits",
+    image: "/assets/fruits/dragonfruit.jpg",
+    value: "dragonfruit",
+  },
+  {
+    type: "fruits",
+    image: "/assets/fruits/grapefruit.jpg",
+    value: "grapefruit",
+  },
+  {
+    type: "fruits",
+    image: "/assets/fruits/grapes.jpg",
+    value: "grape",
+  },
+  {
+    type: "fruits",
+    image: "/assets/fruits/lemon.jpg",
+    value: "lemon",
+  },
+  {
+    type: "fruits",
+    image: "/assets/fruits/lime.jpg",
+    value: "lime",
+  },
+  {
+    type: "fruits",
+    image: "/assets/fruits/oranges.jpg",
+    value: "orange",
+  },
+  {
+    type: "fruits",
+    image: "/assets/fruits/peaches.jpg",
+    value: "peach",
+  },
+  {
+    type: "fruits",
+    image: "/assets/fruits/pear.jpg",
+    value: "pear",
+  },
+  {
+    type: "fruits",
+    image: "/assets/fruits/pineapples.jpg",
+    value: "pineapple",
+  },
+  {
+    type: "fruits",
+    image: "/assets/fruits/plums.jpg",
+    value: "plum",
+  },
+  {
+    type: "fruits",
+    image: "/assets/fruits/raspberries.jpg",
+    value: "raspberry",
+  },
+  {
+    type: "fruits",
+    image: "/assets/fruits/strawberries.jpg",
+    value: "strawberry",
+  },
+  {
+    type: "sports",
+    image: "/assets/sports/americanfootball.jpg",
+    value: "american_football",
+  },
+  {
+    type: "sports",
+    image: "/assets/sports/baseball.jpg",
+    value: "baseball",
+  },
+  {
+    type: "sports",
+    image: "/assets/sports/basketball.jpg",
+    value: "basketball",
+  },
+  {
+    type: "sports",
+    image: "/assets/sports/cricket.jpg",
+    value: "cricket",
+  },
+  {
+    type: "sports",
+    image: "/assets/sports/cycling.jpg",
+    value: "cycling",
+  },
+  {
+    type: "sports",
+    image: "/assets/sports/darts.jpg",
+    value: "darts",
+  },
+  {
+    type: "sports",
+    image: "/assets/sports/f1.jpg",
+    value: "f1",
+  },
+  {
+    type: "sports",
+    image: "/assets/sports/football.jpg",
+    value: "football",
+  },
+  {
+    type: "sports",
+    image: "/assets/sports/hockey.jpg",
+    value: "hockey",
+  },
+  {
+    type: "sports",
+    image: "/assets/sports/running.jpg",
+    value: "running",
+  },
+  {
+    type: "sports",
+    image: "/assets/sports/skiing.jpg",
+    value: "skiing",
+  },
+  {
+    type: "sports",
+    image: "/assets/sports/surfing.jpg",
+    value: "surfing",
+  },
+  {
+    type: "sports",
+    image: "/assets/sports/swimming.jpg",
+    value: "swimming",
+  },
+  {
+    type: "sports",
+    image: "/assets/sports/tabletennis.jpg",
+    value: "table_tennis",
+  },
+  {
+    type: "sports",
+    image: "/assets/sports/tennis.jpg",
+    value: "tennis",
+  },
+  {
+    type: "sports",
+    image: "/assets/sports/volleyball.jpg",
+    value: "volleyball",
+  },
+];
