8440dca71f11b04160f1f2e14f69105607be47ca
= <=>
2024-10-12 10:42:40 +0200

    Render GameStats component

--- src/components/GameScreen/GameScreen.tsx
+++ src/components/GameScreen/GameScreen.tsx
@@ -1,16 +1,18 @@
-import { Board } from "components";
+import { Board, GameStats } from "components";
 import useGameScreen from "./useGameScreen";
-
 const GameScreen = () => {
   const { difficulty, isActive, onItemClick, shuffledDeck } = useGameScreen();
 
   return (
-    <Board
-      deck={shuffledDeck}
-      difficulty={difficulty}
-      isActive={isActive}
-      onItemClick={onItemClick}
-    />
+    <div>
+      <GameStats />
+      <Board
+        deck={shuffledDeck}
+        difficulty={difficulty}
+        isActive={isActive}
+        onItemClick={onItemClick}
+      />
+    </div>
   );
 };
 
