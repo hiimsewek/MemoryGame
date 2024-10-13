5733c80abdc70e9b929edbf4beb441f09e03788e
hiimsewek <sewekwoz@gmail.com>
2024-10-13 14:04:36 +0200

    Add utility functions to save and retrieve data from local storage

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,4 +1,4 @@
-import { Category, Difficulty } from "types";
+import { Category, Difficulty, HistoryItem } from "types";
 
 export const loadDifficulty = () => {
   const storageDifficultyItem = localStorage.getItem(
@@ -14,4 +14,29 @@ export const loadCategory = () => {
   const category = storageCategoryItem || "animals";
 
   return category;
+};
+
+export const getItem = (key: string) => {
+  const item = localStorage.getItem(key);
+
+  if (item && key === "history") {
+    return JSON.parse(item) as HistoryItem[];
+  }
+
+  return item;
+};
+
+export const saveItem = (key: string, value: string) => {
+  localStorage.setItem(key, value);
+};
+
+export const addHistoryItem = (item: HistoryItem) => {
+  const savedItems = getItem("history");
+
+  if (savedItems && Array.isArray(savedItems)) {
+    const stringifiedItem = JSON.stringify([item, ...savedItems]);
+    localStorage.setItem("history", stringifiedItem);
+  } else {
+    saveItem("history", JSON.stringify([item]));
+  }
 };
